8d33f38e3527f55e40f250b4f5246f9db84022cc
Christian C. Romero <id.32@outlook.com>
2021-05-16 16:19:20 +0200

    update

--- esp32/stiring_test.py
+++ esp32/stiring_test.py
@@ -6,46 +6,9 @@ import time
 from utime import ticks_add
 from utime import ticks_diff
 
+import classes
 
-class PrecisionStepper:
-	"""Class for stepper motor"""
-
-	def __init__(self, step_pin, dir_pin, en_pin, step_time=1000, steps_per_rev=1600):
-		"""Initialise stepper."""
-		self.stp = machine.Pin(step_pin, Pin.OUT)
-		self.dir = machine.Pin(dir_pin, Pin.OUT)
-		self.en = machine.Pin(en_pin, Pin.OUT, value=0)
-
-		self.step_time = step_time
-		self.steps_per_rev = steps_per_rev
-
-	def power_on(self):
-		"""Power on stepper."""
-		self.en.value(0)
-
-	def power_off(self):
-		"""Power off stepper."""
-		self.en.value(1)
-
-	def set_dir(self, _dir):
-		self.dir.value(_dir)
-
-	def steps(self, step_count):
-		"""Rotate stepper for given steps."""
-		for i in range(abs(step_count)):
-			self.stp.value(1)
-			thime.sleep_us(self.step_time)
-			self.stp.value(0)
-			thime.sleep_us(self.step_time)
-
-	def mm(self, mm, step_per_mm):
-		self.steps(mm * step_per_mm)
-
-	def set_step_time(self, us):
-		"""Set time in microseconds between each step."""
-		self.step_time = us
-
-stirring_steppers = PrecisionStepper(step_pin=19, dir_pin=21, en_pin=18, step_time=1000)
+stirring_steppers = classes.PrecisionStepper(step_pin=19, dir_pin=21, en_pin=18, step_time=1000)
 
 def stiring(duration_seconds):
 	stirring_steppers.power_on()
