f89e58c9a54a79ac60860950bcff86889fbee7c3
Andreu Bonet <andreu.bonet2@gmail.com>
2021-05-16 16:21:43 +0200

    update boot.py

--- esp32/boot.py
+++ esp32/boot.py
@@ -1,21 +1,5 @@
-import network
-
-
-def wifiConnect(ssid, password):
-	station = network.WLAN(network.STA_IF)
-	
-	if station.isconnected():
-		return station.ifconfig()
-
-	if not station.active():
-		station.active(True)
-	
-	station.connect(ssid, password)
-
-	while not station.isconnected():
-		pass
-
-	return station.ifconfig()
-
-
-wifiConnect('MOVISTAR_DF98', 'pU09K%chTQ2$mMhQ4c0U')
+# This file is executed on every boot (including wake-boot from deepsleep)
+import esp
+esp.osdebug(None)
+import webrepl
+webrepl.start()
